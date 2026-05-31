a73d8f90eb4971e8403a9e8657fac3faa36ffd9b
ManuelRivero100 <manuel.rivero.0608@gmail.com>
2026-05-31 21:19:56 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'irv10-v1';
+const FILES = ['./'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(FILES); })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r || fetch(e.request).catch(function(){
+        return caches.match('./');
+      });
+    })
+  );
+});
