1e984b899caaa87aa922d99d8777db551f3cf288
ManuelRivero100 <manuel.rivero.0608@gmail.com>
2026-07-06 23:55:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — Partes IRV-10
-const CACHE = 'irv10-v20260706g';
+const CACHE = 'irv10-v20260706i';
 const FILES = ['./','./index.html','./manifest.json','./icon.png'];
 self.addEventListener('install', function(e){
   self.skipWaiting();
